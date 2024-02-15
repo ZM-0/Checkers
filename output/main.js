02551355eb8f246223568af4726d7e71b5fa2c9c
ZM-0 <zareef.m@hotmail.com>
2024-02-15 21:08:51 +1100

    Add initial Cell class

--- output/main.js
+++ output/main.js
@@ -14,6 +14,73 @@ var Colour;
 })(Colour || (Colour = {}));
 // ====================================================================================================
 // 
+// Cell
+// 
+// ====================================================================================================
+/**
+ * A cell or position in the 8x8 gameboard.
+ */
+class Cell {
+    /**
+     * The diagonally adjacent cells as a 2D array. The first subarray are cells above,
+     * the second subarray are the cells below. The first in each subarray is the left cell.
+     */
+    _adjacentCells = null;
+    /**
+     * The token on this cell or null if no token is on it.
+     */
+    _token = null;
+    /**
+     * Gets the adjacent cells.
+     */
+    get adjacentCells() {
+        return this._adjacentCells;
+    }
+    /**
+     * Sets the adjacent cells for this cell.
+     */
+    set adjacentCells(cells) {
+        if (this._adjacentCells !== null)
+            return;
+        this._adjacentCells = cells;
+    }
+    /**
+     * Gets the token on the cell, or null if there is no token.
+     */
+    get token() {
+        return this._token;
+    }
+    /**
+     * Sets the token on the cell. Does nothing if there is already a token on it.
+     */
+    set token(token) {
+        if (this._token !== null)
+            return;
+        this._token = token;
+    }
+    /**
+     * Checks if the cell has a token.
+     * @returns A boolean indicating if the cell has a token.
+     */
+    hasToken() {
+        return this._token !== null;
+    }
+    /**
+     * Removes the token from the cell, if any.
+     */
+    removeToken() {
+        this._token = null;
+    }
+}
+// ====================================================================================================
+// 
+// Token
+// 
+// ====================================================================================================
+class Token {
+}
+// ====================================================================================================
+// 
 // Main script
 // 
 // ====================================================================================================
--- source/main.ts
+++ source/main.ts
@@ -16,6 +16,87 @@ enum Colour {
 
 // ====================================================================================================
 // 
+// Cell
+// 
+// ====================================================================================================
+
+
+/**
+ * A cell or position in the 8x8 gameboard.
+ */
+class Cell {
+    /**
+     * The diagonally adjacent cells as a 2D array. The first subarray are cells above,
+     * the second subarray are the cells below. The first in each subarray is the left cell.
+     */
+    private _adjacentCells: Cell[][] | null = null;
+
+    /**
+     * The token on this cell or null if no token is on it.
+     */
+    private _token: Token | null = null;
+
+    /**
+     * Gets the adjacent cells.
+     */
+    public get adjacentCells(): Cell[][] | null {
+        return this._adjacentCells;
+    }
+
+    /**
+     * Sets the adjacent cells for this cell.
+     */
+    public set adjacentCells(cells: Cell[][]) {
+        if (this._adjacentCells !== null) return;
+        this._adjacentCells = cells;
+    }
+
+    /**
+     * Gets the token on the cell, or null if there is no token.
+     */
+    public get token(): Token | null {
+        return this._token;
+    }
+
+    /**
+     * Sets the token on the cell. Does nothing if there is already a token on it.
+     */
+    public set token(token: Token) {
+        if (this._token !== null) return;
+        this._token = token;
+    }
+
+    /**
+     * Checks if the cell has a token.
+     * @returns A boolean indicating if the cell has a token.
+     */
+    public hasToken(): boolean {
+        return this._token !== null;
+    }
+
+    /**
+     * Removes the token from the cell, if any.
+     */
+    public removeToken() {
+        this._token = null;
+    }
+}
+
+
+// ====================================================================================================
+// 
+// Token
+// 
+// ====================================================================================================
+
+
+class Token {
+
+}
+
+
+// ====================================================================================================
+// 
 // Main script
 // 
 // ====================================================================================================
