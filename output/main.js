03e89153df32407e1aebe81ed08341e9edf92493
ZM-0 <zareef.m@hotmail.com>
2024-02-16 15:02:32 +1100

    Update adjacent cell fields and add MoveValidator getting valid moves

--- output/main.js
+++ output/main.js
@@ -53,17 +53,15 @@ class Board {
     assignAdjacentCells() {
         for (let row = 0; row < this.SIZE; row++) {
             for (let column = 0; column < this.SIZE; column++) {
-                const adjacentCells = [
-                    [
-                        row > 0 && column > 0 ? this.cells[row - 1][column - 1] : null,
-                        row > 0 && column < this.SIZE - 1 ? this.cells[row - 1][column + 1] : null
-                    ],
-                    [
-                        row < this.SIZE - 1 && column > 0 ? this.cells[row + 1][column - 1] : null,
-                        row < this.SIZE - 1 && column < this.SIZE - 1 ? this.cells[row + 1][column + 1] : null
-                    ]
-                ];
-                this.cells[row][column].adjacentCells = adjacentCells;
+                const cell = this.cells[row][column];
+                if (row > 0 && column > 0)
+                    cell.topLeft = this.cells[row - 1][column - 1];
+                if (row > 0 && column < this.SIZE - 1)
+                    cell.topRight = this.cells[row - 1][column + 1];
+                if (row < this.SIZE - 1 && column > 0)
+                    cell.bottomLeft = this.cells[row + 1][column - 1];
+                if (row < this.SIZE - 1 && column < this.SIZE - 1)
+                    cell.bottomRight = this.cells[row + 1][column + 1];
             }
         }
     }
@@ -91,28 +89,80 @@ class Board {
  */
 class Cell {
     /**
-     * The diagonally adjacent cells as a 2D array. The first subarray are cells above,
-     * the second subarray are the cells below. The first in each subarray is the left cell.
-     * If an adjacent cell doesn't exist (i.e. corner or edge) the entry is null.
+     * The top-left cell or null if there is none.
      */
-    _adjacentCells = null;
+    _topLeft = null;
+    /**
+     * The top-right cell or null if there is none.
+     */
+    _topRight = null;
+    /**
+     * The bottom-left cell or null if there is none.
+     */
+    _bottomLeft = null;
+    /**
+     * The bottom-right cell or null if there is none.
+     */
+    _bottomRight = null;
     /**
      * The token on this cell or null if no token is on it.
      */
     _token = null;
     /**
-     * Gets the adjacent cells.
+     * Gets the top-left cell.
      */
-    get adjacentCells() {
-        return this._adjacentCells;
+    get topLeft() {
+        return this._topLeft;
     }
     /**
-     * Sets the adjacent cells for this cell.
+     * Sets the top-left cell for this cell. Does nothing if it is already assigned.
      */
-    set adjacentCells(cells) {
-        if (this._adjacentCells !== null)
+    set topLeft(cell) {
+        if (this._topLeft !== null)
             return;
-        this._adjacentCells = cells;
+        this._topLeft = cell;
+    }
+    /**
+     * Gets the top-right cell.
+     */
+    get topRight() {
+        return this._topRight;
+    }
+    /**
+     * Sets the top-right cell for this cell. Does nothing if it is already assigned.
+     */
+    set topRight(cell) {
+        if (this._topRight !== null)
+            return;
+        this._topRight = cell;
+    }
+    /**
+     * Gets the bottom-left cell.
+     */
+    get bottomLeft() {
+        return this._bottomLeft;
+    }
+    /**
+     * Sets the bottom-left cell for this cell. Does nothing if it is already assigned.
+     */
+    set bottomLeft(cell) {
+        if (this._bottomLeft !== null)
+            return;
+        this._bottomLeft = cell;
+    }
+    /**
+     * Gets the bottom-right cell.
+     */
+    get bottomRight() {
+        return this._bottomRight;
+    }
+    /**
+     * Sets the bottom-right cell for this cell. Does nothing if it is already assigned.
+     */
+    set bottomRight(cell) {
+        if (this._bottomRight !== null)
+            return;
+        this._bottomRight = cell;
     }
     /**
      * Gets the token on the cell, or null if there is no token.
@@ -140,6 +190,59 @@ class Cell {
      */
     removeToken() {
         this._token = null;
+    }
+}
+// ====================================================================================================
+// 
+// Player
+// 
+// ====================================================================================================
+/**
+ * A player.
+ */
+class Player {
+    /**
+     * The player's colour.
+     */
+    COLOUR;
+    /**
+     * The player's tokens.
+     */
+    tokens;
+    /**
+     * Creates a new player.
+     * @param colour The player's colour.
+     */
+    constructor(colour) {
+        this.COLOUR = colour;
+        this.createTokens();
+    }
+    /**
+     * Creates the player's tokens and links them to the DOM.
+     */
+    createTokens() {
+    }
+    /**
+     * Checks if the player has lost.
+     * @returns A boolean indicating if the player has lost.
+     */
+    hasLost() {
+        return false;
+    }
+    /**
+     * Checks if all the player's tokens are blocked.
+     * @returns A boolean indicating if all the alive tokens can't move.
+     */
+    isBlocked() {
+        return false;
+    }
+    /**
+     * Resets the player and all its tokens.
+     */
+    reset() {
+        for (const token of this.tokens) {
+            token.reset();
+        }
     }
 }
 // ====================================================================================================
@@ -204,6 +307,78 @@ class Token {
         this.cell?.removeToken();
         this.cell = this.defaultCell;
         this.cell.token = this;
+    }
+}
+// ====================================================================================================
+// 
+// Move Validator
+// 
+// ====================================================================================================
+/**
+ * Gets valid token moves and validates moves.
+ */
+class MoveValidator {
+    /**
+     * Gets the valid moves for a token at a given cell.
+     * @param start The token's cell at the start of the move.
+     * @returns A list of the valid cells to move to.
+     * @throws Error if the start cell doesn't have a token.
+     */
+    getValidMoves(start) {
+        if (!start.hasToken())
+            throw new Error("Need token on cell to find moves");
+        const moves = [];
+        const colour = start.token.getColour();
+        // Check upward cells for moves
+        if (colour === Colour.BLACK || start.token.isKing)
+            moves.push(...this.getValidUpMoves(start));
+        // Check downward cells for moves
+        if (colour == Colour.WHITE || start.token.isKing)
+            moves.push(...this.getValidDownMoves(start));
+        return moves;
+    }
+    /**
+     * Gets the valid moves of a token in the upwards direction.
+     * @param start The token's current cell.
+     */
+    getValidUpMoves(start) {
+        const moves = [];
+        const topLeftMove = this.getValidMove(start, start.topLeft, start.topLeft?.topLeft);
+        if (topLeftMove)
+            moves.push(topLeftMove);
+        const topRightMove = this.getValidMove(start, start.topRight, start.topRight?.topRight);
+        if (topRightMove)
+            moves.push(topRightMove);
+        return moves;
+    }
+    /**
+     * Gets the valid moves of a token in the downwards direction.
+     * @param start The token's current cell.
+     */
+    getValidDownMoves(start) {
+        const moves = [];
+        const bottomLeftMove = this.getValidMove(start, start.bottomLeft, start.bottomLeft?.bottomLeft);
+        if (bottomLeftMove)
+            moves.push(bottomLeftMove);
+        const bottomRightMove = this.getValidMove(start, start.bottomRight, start.bottomRight?.bottomRight);
+        if (bottomRightMove)
+            moves.push(bottomRightMove);
+        return moves;
+    }
+    /**
+     * Finds the valid cell to move to, if any, in a given direction.
+     * The cells in the chosen direction must be explicitly provided.
+     * @param start The token's current cell.
+     * @param next1 The next adjacent cell.
+     * @param next2 The second-next adjacent cell.
+     * @returns The valid cell to move to, or null if there is no valid move in the chosen direction.
+     */
+    getValidMove(start, next1, next2) {
+        if (next1 && !next1.hasToken())
+            return next1;
+        if (next1 && next1.token.getColour() !== start.token.getColour() && next2 && !next2.hasToken())
+            return next2;
+        return null;
     }
 }
 // ====================================================================================================
--- source/main.ts
+++ source/main.ts
@@ -62,18 +62,12 @@ class Board {
     private assignAdjacentCells() {
         for (let row: number = 0; row < this.SIZE; row++) {
             for (let column: number = 0; column < this.SIZE; column++) {
-                const adjacentCells: (Cell | null)[][] = [
-                    [
-                        row > 0 && column > 0 ? this.cells[row - 1][column - 1] : null,
-                        row > 0 && column < this.SIZE - 1 ? this.cells[row - 1][column + 1] : null
-                    ],
-                    [
-                        row < this.SIZE - 1 && column > 0 ? this.cells[row + 1][column - 1] : null,
-                        row < this.SIZE - 1 && column < this.SIZE - 1 ? this.cells[row + 1][column + 1] : null
-                    ]
-                ];
-
-                this.cells[row][column].adjacentCells = adjacentCells;
+                const cell: Cell = this.cells[row][column];
+                
+                if (row > 0 && column > 0) cell.topLeft = this.cells[row - 1][column - 1];
+                if (row > 0 && column < this.SIZE - 1) cell.topRight = this.cells[row - 1][column + 1];
+                if (row < this.SIZE - 1 && column > 0) cell.bottomLeft = this.cells[row + 1][column - 1];
+                if (row < this.SIZE - 1 && column < this.SIZE - 1) cell.bottomRight = this.cells[row + 1][column + 1];
             }
         }
     }
@@ -107,11 +101,24 @@ class Board {
  */
 class Cell {
     /**
-     * The diagonally adjacent cells as a 2D array. The first subarray are cells above,
-     * the second subarray are the cells below. The first in each subarray is the left cell.
-     * If an adjacent cell doesn't exist (i.e. corner or edge) the entry is null.
+     * The top-left cell or null if there is none.
      */
-    private _adjacentCells: (Cell | null)[][] | null = null;
+    private _topLeft: Cell | null = null;
+
+    /**
+     * The top-right cell or null if there is none.
+     */
+    private _topRight: Cell | null = null;
+
+    /**
+     * The bottom-left cell or null if there is none.
+     */
+    private _bottomLeft: Cell | null = null;
+
+    /**
+     * The bottom-right cell or null if there is none.
+     */
+    private _bottomRight: Cell | null = null;
 
     /**
      * The token on this cell or null if no token is on it.
@@ -119,18 +126,63 @@ class Cell {
     private _token: Token | null = null;
 
     /**
-     * Gets the adjacent cells.
+     * Gets the top-left cell.
      */
-    public get adjacentCells(): (Cell | null)[][] | null {
-        return this._adjacentCells;
+    public get topLeft(): Cell | null {
+        return this._topLeft;
     }
 
     /**
-     * Sets the adjacent cells for this cell.
+     * Sets the top-left cell for this cell. Does nothing if it is already assigned.
      */
-    public set adjacentCells(cells: (Cell | null)[][]) {
-        if (this._adjacentCells !== null) return;
-        this._adjacentCells = cells;
+    public set topLeft(cell: Cell) {
+        if (this._topLeft !== null) return;
+        this._topLeft = cell;
+    }
+
+    /**
+     * Gets the top-right cell.
+     */
+    public get topRight(): Cell | null {
+        return this._topRight;
+    }
+
+    /**
+     * Sets the top-right cell for this cell. Does nothing if it is already assigned.
+     */
+    public set topRight(cell: Cell) {
+        if (this._topRight !== null) return;
+        this._topRight = cell;
+    }
+
+    /**
+     * Gets the bottom-left cell.
+     */
+    public get bottomLeft(): Cell | null {
+        return this._bottomLeft;
+    }
+
+    /**
+     * Sets the bottom-left cell for this cell. Does nothing if it is already assigned.
+     */
+    public set bottomLeft(cell: Cell) {
+        if (this._bottomLeft !== null) return;
+        this._bottomLeft = cell;
+    }
+
+    /**
+     * Gets the bottom-right cell.
+     */
+    public get bottomRight(): Cell | null {
+        return this._bottomRight;
+    }
+
+    /**
+     * Sets the bottom-right cell for this cell. Does nothing if it is already assigned.
+     */
+    public set bottomRight(cell: Cell) {
+        if (this._bottomRight !== null) return;
+        this._bottomRight = cell;
     }
 
     /**
@@ -161,6 +213,70 @@ class Cell {
      */
     public removeToken() {
         this._token = null;
+    }
+}
+
+
+// ====================================================================================================
+// 
+// Player
+// 
+// ====================================================================================================
+
+
+/**
+ * A player.
+ */
+class Player {
+    /**
+     * The player's colour.
+     */
+    private COLOUR: Colour;
+
+    /**
+     * The player's tokens.
+     */
+    private tokens!: Token[];
+
+    /**
+     * Creates a new player.
+     * @param colour The player's colour.
+     */
+    constructor(colour: Colour) {
+        this.COLOUR = colour;
+        this.createTokens();
+    }
+
+    /**
+     * Creates the player's tokens and links them to the DOM.
+     */
+    private createTokens() {
+
+    }
+
+    /**
+     * Checks if the player has lost.
+     * @returns A boolean indicating if the player has lost.
+     */
+    public hasLost(): boolean {
+        return false;
+    }
+
+    /**
+     * Checks if all the player's tokens are blocked.
+     * @returns A boolean indicating if all the alive tokens can't move.
+     */
+    private isBlocked(): boolean {
+        return false;
+    }
+
+    /**
+     * Resets the player and all its tokens.
+     */
+    public reset() {
+        for (const token of this.tokens) {
+            token.reset();
+        }
     }
 }
 
@@ -237,6 +353,85 @@ class Token {
         this.cell?.removeToken();
         this.cell = this.defaultCell;
         this.cell.token = this;
+    }
+}
+
+
+// ====================================================================================================
+// 
+// Move Validator
+// 
+// ====================================================================================================
+
+
+/**
+ * Gets valid token moves and validates moves.
+ */
+class MoveValidator {
+    /**
+     * Gets the valid moves for a token at a given cell.
+     * @param start The token's cell at the start of the move.
+     * @returns A list of the valid cells to move to.
+     * @throws Error if the start cell doesn't have a token.
+     */
+    public getValidMoves(start: Cell): Cell[] {
+        if (!start.hasToken()) throw new Error("Need token on cell to find moves");
+        const moves: Cell[] = [];
+        const colour: Colour = start.token!.getColour();
+
+        // Check upward cells for moves
+        if (colour === Colour.BLACK || start.token!.isKing) moves.push(...this.getValidUpMoves(start));
+
+        // Check downward cells for moves
+        if (colour == Colour.WHITE || start.token!.isKing) moves.push(...this.getValidDownMoves(start));
+
+        return moves;
+    }
+
+    /**
+     * Gets the valid moves of a token in the upwards direction.
+     * @param start The token's current cell.
+     */
+    private getValidUpMoves(start: Cell): Cell[] {
+        const moves: Cell[] = [];
+
+        const topLeftMove: Cell | null = this.getValidMove(start, start.topLeft, start.topLeft?.topLeft);
+        if (topLeftMove) moves.push(topLeftMove);
+
+        const topRightMove: Cell | null = this.getValidMove(start, start.topRight, start.topRight?.topRight);
+        if (topRightMove) moves.push(topRightMove);
+
+        return moves;
+    }
+
+    /**
+     * Gets the valid moves of a token in the downwards direction.
+     * @param start The token's current cell.
+     */
+    private getValidDownMoves(start: Cell): Cell[] {
+        const moves: Cell[] = [];
+
+        const bottomLeftMove: Cell | null = this.getValidMove(start, start.bottomLeft, start.bottomLeft?.bottomLeft);
+        if (bottomLeftMove) moves.push(bottomLeftMove);
+
+        const bottomRightMove: Cell | null = this.getValidMove(start, start.bottomRight, start.bottomRight?.bottomRight);
+        if (bottomRightMove) moves.push(bottomRightMove);
+
+        return moves;
+    }
+
+    /**
+     * Finds the valid cell to move to, if any, in a given direction.
+     * The cells in the chosen direction must be explicitly provided.
+     * @param start The token's current cell.
+     * @param next1 The next adjacent cell.
+     * @param next2 The second-next adjacent cell.
+     * @returns The valid cell to move to, or null if there is no valid move in the chosen direction.
+     */
+    private getValidMove(start: Cell, next1: Cell | null | undefined, next2: Cell | null | undefined): Cell | null {
+        if (next1 && !next1.hasToken()) return next1;
+        if (next1 && next1.token!.getColour() !== start.token!.getColour() && next2 && !next2.hasToken()) return next2;
+        return null;
     }
 }
 
