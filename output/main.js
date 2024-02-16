c19cb7b30442a008a39651ecc25f9697768ee477
ZM-0 <zareef.m@hotmail.com>
2024-02-16 12:32:10 +1100

    Add initial Token class

--- output/main.js
+++ output/main.js
@@ -147,7 +147,64 @@ class Cell {
 // Token
 // 
 // ====================================================================================================
+/**
+ * A player's token.
+ */
 class Token {
+    /**
+     * Indicates if the token is alive.
+     */
+    isAlive = true;
+    /**
+     * Indicates if the token is a king.
+     */
+    isKing = false;
+    /**
+     * The token's colour.
+     */
+    COLOUR;
+    /**
+     * The token's initial cell.
+     */
+    defaultCell;
+    /**
+     * The cell the token is on, or null if dead.
+     */
+    cell;
+    /**
+     * Creates a new token.
+     * @param colour The token's colour.
+     * @param cell The cell the token is on.
+     */
+    constructor(colour, cell) {
+        this.COLOUR = colour;
+        this.defaultCell = cell;
+        this.cell = cell;
+    }
+    /**
+     * Gets the token's colour.
+     * @returns The token's colour.
+     */
+    getColour() {
+        return this.COLOUR;
+    }
+    /**
+     * Gets the token's cell.
+     * @returns The token's cell or null if there isn't one.
+     */
+    getCell() {
+        return this.cell;
+    }
+    /**
+     * Resets the token to its initial position and state.
+     */
+    reset() {
+        this.isAlive = true;
+        this.isKing = false;
+        this.cell?.removeToken();
+        this.cell = this.defaultCell;
+        this.cell.token = this;
+    }
 }
 // ====================================================================================================
 // 
--- source/main.ts
+++ source/main.ts
@@ -29,7 +29,7 @@ class Board {
      * The board dimensions.
      */
     private SIZE: number = 8;
-    
+
     /**
      * The cells in the board.
      */
@@ -172,8 +172,72 @@ class Cell {
 // ====================================================================================================
 
 
+/**
+ * A player's token.
+ */
 class Token {
+    /**
+     * Indicates if the token is alive.
+     */
+    public isAlive: boolean = true;
 
+    /**
+     * Indicates if the token is a king.
+     */
+    public isKing: boolean = false;
+
+    /**
+     * The token's colour.
+     */
+    private COLOUR: Colour;
+
+    /**
+     * The token's initial cell.
+     */
+    private defaultCell: Cell;
+
+    /**
+     * The cell the token is on, or null if dead.
+     */
+    private cell: Cell | null;
+
+    /**
+     * Creates a new token.
+     * @param colour The token's colour.
+     * @param cell The cell the token is on.
+     */
+    constructor(colour: Colour, cell: Cell) {
+        this.COLOUR = colour;
+        this.defaultCell = cell;
+        this.cell = cell;
+    }
+
+    /**
+     * Gets the token's colour.
+     * @returns The token's colour.
+     */
+    public getColour(): Colour {
+        return this.COLOUR;
+    }
+
+    /**
+     * Gets the token's cell.
+     * @returns The token's cell or null if there isn't one.
+     */
+    public getCell(): Cell | null {
+        return this.cell;
+    }
+
+    /**
+     * Resets the token to its initial position and state.
+     */
+    public reset() {
+        this.isAlive = true;
+        this.isKing = false;
+        this.cell?.removeToken();
+        this.cell = this.defaultCell;
+        this.cell.token = this;
+    }
 }
 
 
